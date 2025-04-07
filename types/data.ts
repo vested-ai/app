b1c2b4d3e16de5e534efbee49765228d455429a2
Brittany Dufort <brittany.dufort@gmail.com>
2025-04-07 03:47:10 -0400

    data

---
+++ types/data.ts
@@ -0,0 +1,29 @@
+export interface MatchRecommendation {
+    id: string;
+    daterId: string;
+    rankScore: number;
+    name: string;
+    bio: string;
+    image: string;
+}
+
+export interface Dater {
+    id: string;
+    name: string;
+    image: string;
+    bio: string;
+    matchRecommendations: MatchRecommendation[];
+    matchExpirationHrs: number;
+    vestedScore: number;
+    friends: Friend[];
+    reviewingMatches: number;
+}
+
+export interface Friend {
+    id: string;
+    name: string;
+    vestedScore: number;
+    invitedBy: Dater[];
+    daters: Dater[];
+    image: string;
+} 
